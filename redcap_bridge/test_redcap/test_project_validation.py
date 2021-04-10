7cab839e8ec9e11a7ae7ff52249d7521e713f6a2
sprenger <julia.sprenger@univ-amu.fr>
2021-04-10 16:13:43 +0200

    Add validation test and update old tests

--- redcap_bridge/test_redcap/test_project_validation.py
+++ redcap_bridge/test_redcap/test_project_validation.py
@@ -1,9 +1,33 @@
+import pytest
 import pathlib
-from redcap_bridge.redcap_validator import load_template, load_records, validate
+import json
+from redcap_bridge.test_redcap.test_utils import (test_directory,
+                                                  initialize_test_directory,
+                                                  initialize_testfiles)
+from redcap_bridge.project_building import build_project, customize_project
+from redcap_bridge.project_validation import validate_project_against_template_parts, load_template, load_records, validate_record_against_template
+
+project_dir = test_directory / 'testfiles' / 'TestProject'
+
+
+def test_validate_project_against_template_parts(initialize_test_directory, initialize_testfiles):
+    # create the customized project
+    build_project(project_dir / 'structure.csv',
+                  project_dir / 'build.csv')
+    customize_project(project_dir / 'build.csv',
+                      project_dir / 'customizations.csv',
+                      output_file=project_dir / 'customized.csv')
+
+    with open(project_dir / 'project.json') as f:
+        project_dict = json.load(f)
+    template_parts = project_dict['validation']
+
+    validate_project_against_template_parts(project_dir / 'customized.csv',
+                                            *template_parts)
 
 
 def test_load_template():
-    testfile = pathlib.Path('./testfiles/Diglabform_2021-02-15_1731.zip')
+    testfile = test_directory / 'testfiles' / 'Diglabform_2021-02-15_1731.zip'
     template = load_template(testfile)
 
     assert not testfile.with_suffix('').exists()
@@ -30,18 +54,20 @@ def test_load_template():
         assert mandatory_var in template['Variable / Field Name'].values
 
 
+# TODO: Add pytest fixture that ensure that project is defined on server side and contains records
 def test_load_records():
     # NOTE: This test might fail due to connection issues to the server.
     # Ensure to use either lan or wireless connection, but not both at the
     # same time.
-    data = load_records()
+    data = load_records(project_dir / 'project.json')
 
     assert data is not None
 
-
+# TODO: Add pytest.fixture for this test
 def test_validate():
-    records = load_records()
-    template = load_template('testfiles/Diglabform_2021-02-15_1731.zip')
-    template = load_template('testfiles/DiglabformV4a_2021-03-17_1038.zip')
+    records = load_records(project_dir / 'project.json')
+    # This template might be outdated for these records. Add proper pytest.fixture configuration
+    template = load_template(test_directory / 'testfiles' / 'DiglabformV4a_2021-03-17_1038.zip')
 
-    validate(template, records)
+    validate_record_against_template(template, records)
+
