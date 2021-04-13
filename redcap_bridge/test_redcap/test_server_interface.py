994f998324a80bf7ee3cb570fb32647ec8b8e6e8
sprenger <julia.sprenger@univ-amu.fr>
2021-04-13 11:43:58 +0200

    Add test for downloading records from server

--- redcap_bridge/test_redcap/test_server_interface.py
+++ redcap_bridge/test_redcap/test_server_interface.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pytest
 
 import redcap
-from redcap_bridge.server_interface import upload_datadict
+from redcap_bridge.server_interface import upload_datadict, download_records
 from redcap_bridge.test_redcap.test_utils import (test_directory,
                                                   initialize_test_directory,
                                                   initialize_testfiles)
@@ -15,10 +15,21 @@ SERVER_CONFIG_YAML = (
 
 
 @pytest.fixture
-def clean_server():
+def clean_server(initialize_test_directory, initialize_testfiles):
+    """
+    Put testing server in a defined state: only minimal metadata (instruments)
+    and records present
+    """
     # replace existing datadict and record at server by default ones
     config = json.load(open(SERVER_CONFIG_YAML, 'r'))
-    redproj = redcap.Project(config['api_url'], config['api_token'], lazy=False)
+
+    # TODO: Add step 0: Initialize project (activate surveys and repeating
+    #  instruments )
+    #  -> requires extension of PyCap by `import_project_info` method
+
+    # first initialize in lazy mode to configure metadata even if server status
+    # is corrupted
+    redproj = redcap.Project(config['api_url'], config['api_token'], lazy=True)
 
     default_datadict = pd.DataFrame(data=[['record_id', 'my_first_instrument',
                                            'text', 'Record ID'] + [''] * 14],
@@ -43,3 +54,45 @@ def test_upload_datadict(clean_server, initialize_test_directory,
         exp = len(lines) - 1  # header row does not generate a record
 
     assert exp == res
+
+def test_upload_records(clean_server, initialize_test_directory, initialize_testfiles):
+    """
+    TODO: Finally this test should test the corresponding redcap_bridge
+    `upload_records` method instead of pycap itself
+    """
+    # upload data records
+    config = json.load(open(SERVER_CONFIG_YAML, 'r'))
+    redproj = redcap.Project(config['api_url'], config['api_token'], lazy=False)
+
+    upload_datadict(test_directory / 'testfiles' / 'metadata.csv',
+                    SERVER_CONFIG_YAML)
+
+    uploaded_records = pd.read_csv(test_directory / 'testfiles' / 'record.csv',
+                                   index_col=0, dtype='str')
+    redproj.import_records(uploaded_records, format='csv', overwrite='overwrite')
+
+
+def test_download_records(clean_server, initialize_test_directory, initialize_testfiles):
+    # Step 1: uploading records
+    # TODO: This part needs to be updated together with `test_upload_records`
+    # upload data records
+    config = json.load(open(SERVER_CONFIG_YAML, 'r'))
+    redproj = redcap.Project(config['api_url'], config['api_token'], lazy=False)
+
+    upload_datadict(test_directory / 'testfiles' / 'metadata.csv',
+                    SERVER_CONFIG_YAML)
+
+    uploaded_records = pd.read_csv(test_directory / 'testfiles' / 'record.csv',
+                                   index_col=0, dtype='str')
+    redproj.import_records(uploaded_records, format='csv', overwrite='overwrite')
+
+    # Step 2: download data records
+    downloaded_csv = test_directory / 'record_downloaded.csv'
+    download_records(downloaded_csv, SERVER_CONFIG_YAML)
+
+    # Step 3: Compare entries
+    assert downloaded_csv.exists()
+
+    # Step 4: Comparison of values
+    downloaded_records = pd.read_csv(downloaded_csv, dtype='str')
+    uploaded_records.equals(downloaded_records)
