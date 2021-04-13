8513a1cde9659c24b24b0f6e22beb184682d1ef1
sprenger <julia.sprenger@univ-amu.fr>
2021-04-13 13:01:23 +0200

    update requirement handling in setup

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ import os
 with open(os.path.join(os.path.dirname(__file__), "VERSION")) as version_file:
     version = version_file.read().strip()
 
+with open('requirements.txt') as f:
+    requires = f.read().splitlines()
+
 
 setup(
     name="DigLabTools",
@@ -22,7 +25,7 @@ setup(
     author="Julia Sprenger, Jeremy Garcia",
     description="Tools to interact with the DigLab metadata collection standard",
     license='MIT',
-    install_requires=[],
+    install_requires=requires,
     include_package_data=True,
     python_requires='>=3.6',
     extras_require={
