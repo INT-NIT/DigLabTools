dfad78156b1e142a463b47f1e63163deb76c3e1c
sprenger <julia.sprenger@univ-amu.fr>
2021-06-21 16:23:38 +0200

    include provenance tracking feature in redcap_bridge

--- redcap_bridge/project_control.py
+++ redcap_bridge/project_control.py
@@ -6,7 +6,7 @@ from redcap_bridge.project_building import build_project, customize_project
 from redcap_bridge.project_validation import validate_project_against_template_parts
 from redcap_bridge.server_interface import upload_datadict, download_records
 
-def setup_project(proj_folder, working_dir=None):
+def setup_project(proj_folder, working_dir=None, include_provenance=True):
     """
     Build a project csv from its specifications and setup on the server
 
@@ -17,6 +17,9 @@ def setup_project(proj_folder, working_dir=None):
             `structure.csv` and `customizations.csv`
         working_dir: (path)
             directory in which to store temporarily generated project files
+        include_provenance: (bool)
+            include hidden provenance information in project csv.
+            Default: True
     """
 
     if working_dir is None:
@@ -27,7 +30,8 @@ def setup_project(proj_folder, working_dir=None):
     with open(proj_folder / 'project.json') as f:
         proj_conf = json.load(f)
 
-    build_project(proj_folder / 'structure.csv', working_dir / 'build.csv')
+    build_project(proj_folder / 'structure.csv', working_dir / 'build.csv',
+                  include_provenance=include_provenance)
     customize_project(working_dir / 'build.csv',
                       proj_folder / 'customizations.csv',
                       output_file=working_dir / 'customized.csv')
