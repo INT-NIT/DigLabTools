1d43dafa5a9abb48f49f1e046e14ad8f13cb1d15
sprenger <julia.sprenger@univ-amu.fr>
2022-03-22 13:26:22 +0100

    bump required python version 3.8 (pycap 2.0 requirement)

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     license='MIT',
     install_requires=requires,
     include_package_data=True,
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     extras_require={
         'test': ['pytest']
     },
