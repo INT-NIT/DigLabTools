b27e2e26c126a5baa66a317fb37fe790ae21ff03
Killian <74175986+killianrochet@users.noreply.github.com>
2023-02-24 13:50:14 +0100

    Add the user of the ELAB environment in the yaml file.
    Modification of the tests, uses of the configuration files in order to not use raw path anymore

--- redcap_bridge/test_redcap/test_server_elab_interface.py
+++ redcap_bridge/test_redcap/test_server_elab_interface.py
@@ -2,16 +2,16 @@ import pytest
 
 
 from redcap_bridge.server_elab_interface import (get_elab_config, create_template_without_metadata)
+from redcap_bridge.test_redcap.test_utils import (test_directory, initialize_test_dir)
 
-
-json_file = "/Users/killianrochet/PycharmProjects/DigLabTools/redcap_bridge/test_redcap/testfiles/TestProject/template.json"
-
-elab_config = "/Users/killianrochet/PycharmProjects/DigLabTools/redcap_bridge/test_redcap/testfiles/TestProject/elab_config.json"
+SERVER_CONFIG_YAML = (test_directory / 'testfiles' / 'TestProject' / 'elab_config.json').resolve()
 
 
 def test_create_template_without_metadata():
 
-    res = create_template_without_metadata(server_config_json=elab_config, template_file=json_file)
+    template_file = test_directory / 'testfiles' / 'elab_template.json'
+
+    res = create_template_without_metadata(server_config_json=SERVER_CONFIG_YAML, template_file=template_file)
 
     assert res is not None
 
