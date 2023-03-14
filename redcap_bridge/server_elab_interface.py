88e075af76502acafb0fbf33343e8cd03e16ae8d
Killian <74175986+killianrochet@users.noreply.github.com>
2023-03-14 15:03:57 +0100

    Implementation of the function to create a template on Elab with an old CSV template file
    The function is useful to allow interoperability between DigLabTool and ElabFTW.
    
    The function takes an old CSV template from RedCap, converts it to a json file and creates a template usable on Elab

--- redcap_bridge/server_elab_interface.py
+++ redcap_bridge/server_elab_interface.py
@@ -1,10 +1,9 @@
-import os
-import warnings
 import json
+import os
 
-import pandas as pd
 import elabapi_python
-from elabapi_python.rest import ApiException
+
+from redcap_bridge.utils import conversion_csv_to_json
 
 
 def create_template_with_metadata(server_config_json, template_file):
@@ -70,6 +69,40 @@ def create_template_without_metadata(server_config_json, template_file):
         location_response = response[2].get('Location')
     else:
         raise ValueError(f'Unknown format or template. Valid format is "json".')
+
+    return location_response
+
+
+def create_template_with_converted_csv(server_config_json, csv_file, title):
+    """
+    Creating a template with an old csv file.
+
+    Parameters
+    ----------
+    server_config_json: str
+        Path to the json file containing the redcap url, api token and required external modules
+    template_file: str
+        Path to the csv file you want to convert into an Elab version
+    title: str
+        Title of the template created
+
+    Returns
+    -------
+        location_response: location of the new template
+
+    """
+
+    json_conversion_file = conversion_csv_to_json(csv_file)
+    json_conversion_file = json.dumps(json_conversion_file)
+
+    api_client = get_elab_config(server_config_json)
+    template_api = elabapi_python.ExperimentsTemplatesApi(api_client)
+
+    response = template_api.post_experiment_template_with_http_info(body={"title": title})
+    location_response = response[2].get('Location')
+    item_id = int(location_response.split('/').pop())
+
+    template_api.patch_experiment_template(item_id, body={'metadata': json_conversion_file})
 
     return location_response
 
--- redcap_bridge/test_redcap/test_server_elab_interface.py
+++ redcap_bridge/test_redcap/test_server_elab_interface.py
@@ -1,7 +1,10 @@
-from redcap_bridge.server_elab_interface import (create_template_without_metadata, create_template_with_metadata)
-from redcap_bridge.test_redcap.test_utils import (test_directory, initialize_test_dir)
+from redcap_bridge.server_elab_interface import (create_template_without_metadata, create_template_with_metadata,
+                                                 create_template_with_converted_csv)
+from redcap_bridge.test_redcap.test_utils import (test_directory)
 
 SERVER_CONFIG_YAML = (test_directory / 'testfiles' / 'TestProject' / 'elab_config.json').resolve()
+
+test_file = (test_directory / 'testfiles' / 'elabConversion' / 'csvRecord.csv')
 
 
 def test_create_template_without_metadata(initialize_test_dir):
@@ -16,5 +19,14 @@ def test_create_template_with_metadata(initialize_test_dir):
     template_file = test_directory / 'testfiles' / 'elab_template.json'
 
     res = create_template_with_metadata(server_config_json=SERVER_CONFIG_YAML, template_file=template_file)
+
+    assert res is not None
+
+
+def test_create_template_with_converted_csv(initialize_test_dir):
+    csv_file = test_directory / 'testfiles' / 'elabConversion' / 'csvRecord.csv'
+
+    res = create_template_with_converted_csv(server_config_json=SERVER_CONFIG_YAML, csv_file=csv_file,
+                                             title='TEST AUTO')
 
     assert res is not None
