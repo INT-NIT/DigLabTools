6869589af678db4749976dcdd51bcb6a5fc7130d
Killian <74175986+killianrochet@users.noreply.github.com>
2023-03-16 10:50:06 +0100

    Fix import initialize_test_dir issue.

--- redcap_bridge/test_redcap/test_server_elab_interface.py
+++ redcap_bridge/test_redcap/test_server_elab_interface.py
@@ -1,6 +1,6 @@
 from redcap_bridge.server_elab_interface import (create_template_without_metadata, create_template_with_metadata,
                                                  create_template_with_converted_csv)
-from redcap_bridge.test_redcap.test_utils import (test_directory)
+from redcap_bridge.test_redcap.test_utils import (test_directory, initialize_test_dir)
 
 SERVER_CONFIG_YAML = (test_directory / 'testfiles' / 'TestProject' / 'elab_config.json').resolve()
 
@@ -24,9 +24,10 @@ def test_create_template_with_metadata(initialize_test_dir):
 
 
 def test_create_template_with_converted_csv(initialize_test_dir):
-    csv_file = test_directory / 'testfiles' / 'elabConversion' / 'csvRecord.csv'
+    csv_file = test_directory / 'testfiles' / 'metadata.csv'
+
 
     res = create_template_with_converted_csv(server_config_json=SERVER_CONFIG_YAML, csv_file=csv_file,
-                                             title='TEST AUTO')
+                                             title='AUTO CSV')
 
     assert res is not None
