d95706d63bbd156d9f3290e862920089a4bbe1cd
Killian <74175986+killianrochet@users.noreply.github.com>
2023-04-07 14:38:15 +0200

    Print statement to see if variable are available in environ

--- redcap_bridge/server_elab_interface.py
+++ redcap_bridge/server_elab_interface.py
@@ -117,8 +117,7 @@ def get_elab_config(server_config_json):
     config = json.load(open(server_config_json, 'r'))
     configuration = elabapi_python.Configuration()
 
-    if config['api_elab_key'] in os.environ:
-        print("Key in os environ")
+    print(os.environ.keys())
 
     if config['api_elab_key'] in os.environ:
         configuration.api_key['api_key'] = os.environ[config['api_elab_key']]
