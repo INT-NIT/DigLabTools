07c60ecbe57ec41e8725c910d54685896da99a29
Killian <74175986+killianrochet@users.noreply.github.com>
2023-05-22 14:27:07 +0200

    Modification of the CLI:
    Removed the addition of a new parser.
    
    Now only one option added to define if we want to use the RedCap server or the ElabFTW server

--- redcap_bridge/cli.py
+++ redcap_bridge/cli.py
@@ -11,7 +11,7 @@ def main(command_line=None):
     import argparse
 
     # create a parser object
-    parser = argparse.ArgumentParser(description="A simple Python interface for RedCap and ElabFTW")
+    parser = argparse.ArgumentParser(description="A simple Python interface for RedCap and elabftw")
 
     parser.add_argument(
         '--debug',
@@ -19,50 +19,40 @@ def main(command_line=None):
         help='Print debug info'
     )
 
-    parser.add_argument('--redcap', action='store_true', help='Use RedCap')
-    parser.add_argument('--elabftw', action='store_true', help='Use ElabFTW')
+    parser.add_argument(
+        'command',
+        choices=['redcap', 'elabftw'],
+        help='Choose the server to download from (redcap or elabftw)'
+    )
 
-    subparsers = parser.add_subparsers(dest='command')
+    parser.add_argument("destination", metavar='destination', type=str,
+                        help="The destination filename.")
+    parser.add_argument("config_json", metavar='config_json', type=str,
+                        help="The json configuration file of the project")
+    parser.add_argument("-f", "--format", type=str, metavar='format',
+                        help="Format to store the data (json/csv)")
+    parser.add_argument("-c", "--compressed", action='store_true',
+                        help="Compress the output file (use labels and merge checkbox columns)")
+    parser.add_argument("experiment_id", metavar='experiment', type=str,
+                        help="Experiment id")
 
-    download = subparsers.add_parser('download', help='Downloads the data records')
-    download.add_argument("destination", nargs=1, metavar='destination', type=str,
-                          help="The destination filename.")
-    download.add_argument("config_json", nargs=1, metavar='config_json', type=str,
-                          help="The json configuration file of the project")
-    download.add_argument("-f", "--format", type=str, nargs=1, metavar='format',
-                          help="Format to store the data (json/csv)")
-    download.add_argument("-c", "--compressed", action='store_true',
-                          help="Compress the output file (use labels and merge checkbox columns)")
-
-    # Options for downloading Elab. To be modified afterwards
-    elabftw = subparsers.add_parser('elabftw', help='Downloads the data records from ElabFTW')
-    elabftw.add_argument("config_json", nargs=1, metavar='config_json', type=str,
-                         help="The json configuration file of the project")
-    elabftw.add_argument("experiment_id", nargs=1, metavar='config_json', type=str,
-                         help="The experiment id")
-
+    # parse arguments
     args = parser.parse_args(command_line)
 
     if args.debug:
         print("debug: " + str(args))
 
-    # We need to have 1 option
-    if not args.redcap and not args.elabftw:
-        parser.error("Please specify either --redcap or --elabftw")
-
-    # Only elab or redcap as options choosing both is not possible
-    if args.redcap and args.elabftw:
-        parser.error("Please specify only one of --redcap or --elabftw")
-
-    if args.command == 'download':
+    if args.command == 'redcap':
         if not args.format:
-            args.format = ['csv']
+            args.format = 'csv'
 
-        if args.redcap:
-            download_records(args.destination[0], args.config_json[0], format=args.format[0],
-                             compressed=bool(args.compressed))
-        elif args.elabftw:
-            download_experiment(args.config_json[0], args.experiment_id[0])
+        download_records(args.destination, args.config_json, format=args.format,
+                         compressed=bool(args.compressed))
+    elif args.command == 'elabftw':
+        if not args.format:
+            args.format = 'csv'
+
+        download_experiment(args.config_json, args.experiment_id)
 
 
 if __name__ == '__main__':
