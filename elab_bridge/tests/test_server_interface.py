9dca82f0262bb6398d65cee1c2f5d73ee8bfb523
Killian <74175986+killianrochet@users.noreply.github.com>
2023-12-20 13:11:16 +0100

    Add downloaded experiment file for the test

--- elab_bridge/tests/test_server_interface.py
+++ elab_bridge/tests/test_server_interface.py
@@ -55,7 +55,7 @@ def test_download_experiment(initialize_test_dir):
 
 
 def test_extended_download(initialize_test_dir):
-    json_file = test_directory / 'testfiles_elab' / 'downloaded_experiment.json'
+    json_file = test_directory / 'testfiles_elab' / 'downloaded_multiple_experiment.json'
 
     experiment = extended_download(save_to=json_file, server_config_json=SERVER_CONFIG_YAML,
                                    experiment_tags=['BIDS'])
