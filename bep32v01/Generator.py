91e755640b7af8d07e0ff6b5dcc1a984326d6bb0
fatai <sifaoufatai@gmail.com>
2024-04-03 10:49:30 +0200

    builder

---
+++ bep32v01/Generator.py
@@ -0,0 +1,19 @@
+import sys
+
+from Createfile import CreatFile
+from Createdirectory import Createdirectory
+
+
+class Generator:
+    def __init__(self, outpout):
+        self.outpout = outpout
+        self.directoy_builder = Createdirectory(outpout)
+        self.file_builder = CreatFile(outpout)
+
+    def generate(self):
+        self.directoy_builder.build()
+        self.file_builder.build()
+if __name__ == "__main__":
+    outpout = input("Enter the output folder path: ")
+    generator = Generator(outpout)
+    generator.generate()
